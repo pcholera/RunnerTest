79a81695d6beeff3901d26af227f2b4147a4e91d
Parshad Cholera <93930475+pcholera@users.noreply.github.com>
2022-03-31 22:08:37 +0530

    Update Program.cs

--- RunnerTest/Program.cs
+++ RunnerTest/Program.cs
@@ -9,7 +9,7 @@ namespace RunnerTest
             BankAccount ba = new BankAccount("Parshad Cholera", 20);
 
             ba.Credit(5);
-            ba.Debit(10);
+            ba.Debit(11);
             Console.WriteLine("Current balance is ${0}", ba.Balance);
         }
     }
